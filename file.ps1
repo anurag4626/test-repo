4c1162d4f2447292cf001395b7e164aa1e18fcea
anurag4626 <anurag.sharma4626@gmail.com>
2022-12-09 15:11:56 +0530

    v0.1

---
+++ file.ps1
@@ -0,0 +1,14 @@
+﻿configuration installingwindowsfeature
+{
+    node ("Node1","Node2","Node3")
+    {
+        File FriendlyName
+        {
+            Ensure          = "Present"
+            SourcePath      = $SourcePath
+            DestinationPath = $DestinationPath
+            Type            = "Directory"
+            DependsOn       = "[WindowsFeature]FriendlyName"
+        }  
+    }
+}
